aca18229491a450a25092c142bc0f3955df4e5af
Tony Wong <realtony@hku.hk>
2021-04-13 10:13:35 +0800

    u

--- web/app.py
+++ web/app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 #db connection 
 mongo = pymongo.MongoClient("mongodb+srv://iimt:iimt@cluster0.xsglo.mongodb.net/iimtnewsdb?retryWrites=true&w=majority") #connect mongodb
 db = mongo.iimtnewsdb #iimtnewsdb is my database
-
+col = db.cluster
+graphs = {"image_2021-01-05all.png", "image_2021-02-01month.png", "image_2021-03-01month.png"}
     
 today = '2021-01-17' #datetime.date.strftime(datetime.date.today(),"%Y-%m-%d") change it to real today in real implementation
 no_error = True #state whether search has error or not 
@@ -28,19 +29,27 @@ def updateDateByMonth(dateresult):
     dateresult = date.strftime("%Y") + "-" + date.strftime("%m") + "-" + date.strftime("%d")
     return dateresult
 
+def returnAll():
+    nc = col.find_one({"cluster_type": "all" }) 
+    img = getImage("2021-01-05", "all")
+    return render_template('index.html', nc=nc, img=img)
+
 def handleError():
     nc = col.find_one({"cluster_type": "all"}) #if search error occur, the web will show current week's cluster
     return render_template('index.html', nc=nc, no_error=False)
 
+def getImage(date, cluster_type):
+    img = "image_" + date + cluster_type + ".png"
+    return "static/" + img if img in graphs else None
+
+
 @app.route('/', methods=['POST','GET'])
 def index():
-    col = db.cluster
     if request.method == 'POST':
         try: 
             typeresult = request.values["ctype"] 
             if typeresult == "all":
-                nc = col.find_one({"cluster_type": typeresult }) 
-                return render_template('index.html', nc=nc, today=today)
+                return returnAll()
             dateresult = request.values["inputdate"]
             if dateresult is "":
                 return handleError()
@@ -49,14 +58,14 @@ def index():
             if typeresult == "month":
                 dateresult = updateDateByMonth(dateresult)
             nc = col.find_one({"cluster_type": typeresult , "date": dateresult }) 
-            return render_template('index.html', nc=nc, today=today)
+            img = getImage(dateresult, typeresult)
+            return render_template('index.html', nc=nc, img=img)
 
         except: 
             return handleError()
         
     else:
-        nc = col.find_one({"cluster_type": "all"}) #default / landing page will show current week's cluster
-        return render_template('index.html', nc=nc)
+        return returnAll()
 
 @app.route('/about', methods=['GET'])
 def about():
